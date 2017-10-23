91a0b490563a16b765e949626db97378fa11a8d1
Ilya Drabenia <ilya.drabenia@altoros.com>
2017-10-23 17:20:57 +0300

    Remove response time bottleneck fix

--- src/util.ts
+++ src/util.ts
@@ -3,27 +3,3 @@
 export function formatPath(contractPath: string) {
     return contractPath.replace(/\\/g, '/');
 }
-
-
-export function throttle(fn, threshhold): Function {
-    threshhold || (threshhold = 250);
-    var last, deferTimer;
-
-    return function () {
-        var context = this;
-  
-        var now = +new Date,
-            args = arguments;
-        if (last && now < last + threshhold) {
-            // hold on to it
-            clearTimeout(deferTimer);
-            deferTimer = setTimeout(function () {
-                last = now;
-                fn.apply(context, args);
-            }, threshhold + last - now);
-        } else {
-            last = now;
-            fn.apply(context, args);
-        }
-    };
-}
