fb449c53c71ae6dc17c0b95c5e0b54c2264ba0b7
Mirko Garozzo <mirkogarozzo@hotmail.it>
2019-09-18 11:21:33 -0500

    Fixed compilation method

--- src/analysers/mythx/login/getCredentials.ts
+++ src/analysers/mythx/login/getCredentials.ts
@@ -4,10 +4,16 @@ import { Credentials } from '../utils/types';
 export async function getCredentials(): Promise<Credentials> {
         const {window} = vscode;
     try {
+<<<<<<< HEAD
             let ethAddress = '0x0000000000000000000000000000000000000000';
             let password = 'trial';
             const projectConfiguration: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration('mythxvsc');
             console.log(projectConfiguration, 'poro');
+=======
+			let ethAddress = "0x0000000000000000000000000000000000000000"
+			let password = "trial"
+			const projectConfiguration: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration('mythxvsc');
+>>>>>>> Fixed compilation method
 
             if (projectConfiguration.ethAddress && projectConfiguration.password) {
                 ethAddress = projectConfiguration.ethAddress;
