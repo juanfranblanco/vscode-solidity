2b1c60f5edf71ffd9d48a4ebecbeb328f05dfb08
Juan Blanco <juanfranblanco@hotmail.com>
2021-01-06 11:14:35 +0000

    Fix new error diagnostics for 0.8.0 #205

--- src/solErrorsToDiagnostics.ts
+++ src/solErrorsToDiagnostics.ts
@@ -20,37 +20,76 @@ export interface CompilerError {
     }
 
     export function errorToDiagnostic(error: any): CompilerError {
-        const errorSplit = error.formattedMessage.split(':');
-        let fileName = errorSplit[0];
-        let index = 1;
 
-        // a full path in windows includes a : for the drive
-        if (process.platform === 'win32') {
-            fileName = errorSplit[0] + ':' + errorSplit[1];
-            index = 2;
-        }
+        if(error.sourceLocation.file !== undefined || error.sourceLocation.file !== null) {
+            let fileName = error.sourceLocation.file;
+            
+            const errorMessage = error.message;
+            const severity = this.getDiagnosticSeverity(error.severity);
+            const errorSplit = error.formattedMessage.substr(error.formattedMessage.indexOf(fileName)).split(':');
+            let index = 1;
+             // a full path in windows includes a : for the drive
+             if (process.platform === 'win32') {
+                index = 2;
+            }
 
-        // tslint:disable-next-line:radix
-        const line = parseInt(errorSplit[index]);
-        // tslint:disable-next-line:radix
-        const column = parseInt(errorSplit[index + 1]);
-        const severity = this.getDiagnosticSeverity(error.severity);
-        const errorMessage = error.message;
-        return {
-            diagnostic: {
-                message: errorMessage,
-                range: {
-                    end: {
-                        character: column + error.sourceLocation.end - error.sourceLocation.start - 1,
-                        line: line - 1,
+            // tslint:disable-next-line:radix
+            const line = parseInt(errorSplit[index]);
+            // tslint:disable-next-line:radix
+            const column = parseInt(errorSplit[index + 1]);
+            return {
+                diagnostic: {
+                    message: errorMessage,
+                    range: {
+                        end: {
+                            character: column + error.sourceLocation.end - error.sourceLocation.start - 1,
+                            line: line - 1,
+                        },
+                        start: {
+                            character: column - 1,
+                            line: line - 1,
+                        },
                     },
-                    start: {
-                        character: column - 1,
-                        line: line - 1,
-                    },
+                    severity: severity,
                 },
-                severity: severity,
-            },
-            fileName: fileName,
-        };
+                fileName: fileName,
+            };
+
+        } else {
+
+            const errorSplit = error.formattedMessage.split(':');
+            let fileName = errorSplit[0];
+            let index = 1;
+
+            // a full path in windows includes a : for the drive
+            if (process.platform === 'win32') {
+                fileName = errorSplit[0] + ':' + errorSplit[1];
+                index = 2;
+            }
+
+            // tslint:disable-next-line:radix
+            const line = parseInt(errorSplit[index]);
+            // tslint:disable-next-line:radix
+            const column = parseInt(errorSplit[index + 1]);
+            const severity = this.getDiagnosticSeverity(error.severity);
+            const errorMessage = error.message;
+            return {
+                diagnostic: {
+                    message: errorMessage,
+                    range: {
+                        end: {
+                            character: column + error.sourceLocation.end - error.sourceLocation.start - 1,
+                            line: line - 1,
+                        },
+                        start: {
+                            character: column - 1,
+                            line: line - 1,
+                        },
+                    },
+                    severity: severity,
+                },
+                fileName: fileName,
+            };
+
+        }
     }
