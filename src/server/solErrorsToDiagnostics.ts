12547ae5eae845b2286bcabd45d5e1f2a5be9a86
Juan Blanco <juanfranblanco@hotmail.com>
2021-11-05 09:50:22 +0000

    #294 Fixed solErrorsToDiagnostics

--- src/server/solErrorsToDiagnostics.ts
+++ src/server/solErrorsToDiagnostics.ts
@@ -21,11 +21,9 @@ export interface CompilerError {
 
     export function errorToDiagnostic(error: any): CompilerError {
 
-        if(error.sourceLocation.file !== undefined || error.sourceLocation.file !== null) {
+        if(error.sourceLocation.file !== undefined && error.sourceLocation.file !== null) {
             let fileName = error.sourceLocation.file;
             
-            const errorMessage = error.message;
-            const severity = this.getDiagnosticSeverity(error.severity);
             const errorSplit = error.formattedMessage.substr(error.formattedMessage.indexOf(fileName)).split(':');
             let index = 1;
              // a full path in windows includes a : for the drive
@@ -33,27 +31,7 @@ export interface CompilerError {
                 index = 2;
             }
 
-            // tslint:disable-next-line:radix
-            const line = parseInt(errorSplit[index]);
-            // tslint:disable-next-line:radix
-            const column = parseInt(errorSplit[index + 1]);
-            return {
-                diagnostic: {
-                    message: errorMessage,
-                    range: {
-                        end: {
-                            character: column + error.sourceLocation.end - error.sourceLocation.start - 1,
-                            line: line - 1,
-                        },
-                        start: {
-                            character: column - 1,
-                            line: line - 1,
-                        },
-                    },
-                    severity: severity,
-                },
-                fileName: fileName,
-            };
+            return splitErrorToDiagnostic(error, errorSplit, index, fileName);
 
         } else {
 
@@ -67,29 +45,34 @@ export interface CompilerError {
                 index = 2;
             }
 
-            // tslint:disable-next-line:radix
-            const line = parseInt(errorSplit[index]);
-            // tslint:disable-next-line:radix
-            const column = parseInt(errorSplit[index + 1]);
-            const severity = this.getDiagnosticSeverity(error.severity);
-            const errorMessage = error.message;
-            return {
-                diagnostic: {
-                    message: errorMessage,
-                    range: {
-                        end: {
-                            character: column + error.sourceLocation.end - error.sourceLocation.start - 1,
-                            line: line - 1,
-                        },
-                        start: {
-                            character: column - 1,
-                            line: line - 1,
-                        },
-                    },
-                    severity: severity,
-                },
-                fileName: fileName,
-            };
+            return splitErrorToDiagnostic(error, errorSplit, index, fileName);
 
         }
     }
+
+    function splitErrorToDiagnostic(error: any, errorSplit: any, index: number, fileName: any):  CompilerError {
+        const severity = this.getDiagnosticSeverity(error.severity);
+        const errorMessage = error.message;
+        // tslint:disable-next-line:radix
+        const line = parseInt(errorSplit[index]);
+        // tslint:disable-next-line:radix
+        const column = parseInt(errorSplit[index + 1]);
+        return {
+            diagnostic: {
+                message: errorMessage,
+                range: {
+                    end: {
+                        character: column + error.sourceLocation.end - error.sourceLocation.start - 1,
+                        line: line - 1,
+                    },
+                    start: {
+                        character: column - 1,
+                        line: line - 1,
+                    },
+                },
+                severity: severity,
+            },
+            fileName: fileName,
+        };
+    }
+
