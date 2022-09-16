875e99f5fe227b4d5364ee07d981b8c63367c08d
Alexey Shekhirin <a.shekhirin@gmail.com>
2022-09-16 18:25:35 +0100

    feat(client, formatter): set forge fmt cwd to respect foundry.toml

--- src/client/formatter/forgeFormatter.ts
+++ src/client/formatter/forgeFormatter.ts
@@ -7,13 +7,21 @@ export async function formatDocument(document: vscode.TextDocument, context: vsc
   const fullTextRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
 
   const formatted = await new Promise<string>((resolve, reject) => {
-    const forge = cp.execFile('forge', ['fmt', '--raw', '-'], (err, stdout) => {
-      if (err !== null) {
-        return reject(err);
-      }
+    const workspaceFolder = vscode.workspace.workspaceFolders?.find((folder) =>
+        document.uri.fsPath.startsWith(folder?.uri.fsPath)
+    );
+    const forge = cp.execFile(
+        'forge',
+        ['fmt', '--raw', '-'],
+        { cwd: workspaceFolder?.uri?.fsPath },
+        (err, stdout) => {
+          if (err !== null) {
+            return reject(err);
+          }
 
-      resolve(stdout);
-    });
+          resolve(stdout);
+        }
+    );
 
     forge.stdin?.write(document.getText());
     forge.stdin?.end();
