b816de285ead8e8061f72ba592114d0c0f1df5d5
Juan Blanco <juanfranblanco@hotmail.com>
2024-01-31 15:21:54 +0000

    adding source fix all

--- src/client/codeActionProviders/addressChecksumActionProvider.ts
+++ src/client/codeActionProviders/addressChecksumActionProvider.ts
@@ -6,6 +6,7 @@ export class AddressChecksumCodeActionProvider implements vscode.CodeActionProvi
 
         public static readonly providedCodeActionKinds = [
             vscode.CodeActionKind.QuickFix,
+            vscode.CodeActionKind.SourceFixAll,
             vscode.CodeActionKind.Empty,
         ];
 
